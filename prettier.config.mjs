ab1ddf91d811a688a4b07a5d8d190f2c73020fcb
Jordi Sala Morales <jordism91@gmail.com>
2024-03-26 17:25:25 +0000

    Configure linters

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
+
+export default config;
--- src/code.ts
+++ src/code.ts
@@ -1,28 +1,25 @@
-
-
-let fileData = [];
-
 type NodeData = {
-  id: string
-  name: string,
-  type: string,
-  children: Node[],
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  fills: any
-}
+  id: string;
+  name: string;
+  type: string;
+  children: Node[];
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  fills: any;
+};
 
 const signatures = {
-  R0lGODdh: "image/gif",
-  R0lGODlh: "image/gif",
-  iVBORw0KGgo: "image/png",
-  "/9j/": "image/jpg"
+  'R0lGODdh': 'image/gif',
+  'R0lGODlh': 'image/gif',
+  'iVBORw0KGgo': 'image/png',
+  '/9j/': 'image/jpg'
 };
 
 function detectMimeType(b64) {
-  for (var s in signatures) {
+  for (const s in signatures) {
     if (b64.indexOf(s) === 0) {
       return signatures[s];
     }
@@ -30,12 +27,13 @@ function detectMimeType(b64) {
 }
 
 function traverse(node): NodeData {
-  let children:any[] = [];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const children: any[] = [];
 
-  if ("children" in node) {
-    if (node.type !== "INSTANCE") {
+  if ('children' in node) {
+    if (node.type !== 'INSTANCE') {
       for (const child of node.children) {
-        children.push (traverse(child))
+        children.push(traverse(child));
       }
     }
   }
@@ -50,31 +48,42 @@ function traverse(node): NodeData {
     width: node.width,
     height: node.height,
     fills: node.fills === figma.mixed ? [] : node.fills //TODO: Support mixed fills
-  }
+  };
 
-  if (node.fills && Array.isArray(node.fills)){
-
+  if (node.fills && Array.isArray(node.fills)) {
     // Find any fill of type image
-    const imageFill = node.fills.find(fill => fill.type === "IMAGE");
+    const imageFill = node.fills.find(fill => fill.type === 'IMAGE');
     if (imageFill) {
       // An "image" in Figma is a shape with one or more image fills, potentially blended with other fill
       // types.  Given the complexity of mirroring this exactly in Penpot, which treats images as first-class
       // objects, we're going to simplify this by exporting this shape as a PNG image.
-      node.exportAsync({format: "PNG"}).then((value) => {
+      node.exportAsync({ format: 'PNG' }).then(value => {
         const b64 = figma.base64Encode(value);
-        figma.ui.postMessage({type: "IMAGE", data: {
-          id: node.id,
-          value: "data:" + detectMimeType(b64) + ";base64," + b64
-        }});
+        figma.ui.postMessage({
+          type: 'IMAGE',
+          data: {
+            id: node.id,
+            value: 'data:' + detectMimeType(b64) + ';base64,' + b64
+          }
+        });
       });
     }
   }
 
-  if (node.type == "TEXT") {
-    const styledTextSegments = node.getStyledTextSegments(["fontName", "fontSize", "fontWeight", "lineHeight", "letterSpacing", "textCase", "textDecoration", "fills"]);
+  if (node.type == 'TEXT') {
+    const styledTextSegments = node.getStyledTextSegments([
+      'fontName',
+      'fontSize',
+      'fontWeight',
+      'lineHeight',
+      'letterSpacing',
+      'textCase',
+      'textDecoration',
+      'fills'
+    ]);
 
     if (styledTextSegments[0]) {
-      let font = {
+      const font = {
         fontName: styledTextSegments[0].fontName,
         fontSize: styledTextSegments[0].fontSize.toString(),
         fontWeight: styledTextSegments[0].fontWeight.toString(),
@@ -88,7 +97,7 @@ function traverse(node): NodeData {
         textAlignVertical: node.textAlignVertical,
         children: styledTextSegments
       };
-      result = {...result, ...font};
+      result = { ...result, ...font };
     }
   }
 
@@ -97,16 +106,15 @@ function traverse(node): NodeData {
 
 figma.showUI(__html__, { themeColors: true, height: 200, width: 300 });
 
-let root: NodeData = traverse(figma.root) // start the traversal at the root
-figma.ui.postMessage({type: "FIGMAFILE", data: root});
+const root: NodeData = traverse(figma.root); // start the traversal at the root
+figma.ui.postMessage({ type: 'FIGMAFILE', data: root });
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === "cancel") {
+figma.ui.onmessage = msg => {
+  if (msg.type === 'cancel') {
     figma.closePlugin();
   }
 
-  if (msg.type === "resize") {
+  if (msg.type === 'resize') {
     figma.ui.resize(msg.width, msg.height);
   }
-
 };
